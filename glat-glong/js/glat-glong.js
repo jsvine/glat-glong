5baeff8cab076d3beeb88d96e5f609a0420b534b
Jeremy Singer-Vine <jsvine@gmail.com>
2014-02-03 23:59:00 -0500

    Handle new-new URL structure and refactor

--- glat-glong/js/glat-glong.js
+++ glat-glong/js/glat-glong.js
@@ -33,15 +33,35 @@
 	dotInner.setAttribute("title", "Click for coordinates.");
 
 	var get_old_ll = function () {
-		var link = document.getElementById("link");	
+		var link = document.getElementById("link");
 		var href = link.getAttribute("href");
 		return href.match(/&ll=(-?[\d\.]+),(-?[\d\.]+)/).slice(1);
 	};
 
+	var url_parsers = [
+		{
+			pattern: /\/@/,
+			parser: function (url) {
+				var post_aerobase = url.split("@")[1];
+				var comma_sep = post_aerobase.split(",");
+				return comma_sep.slice(0, 2);
+			}
+		},
+		{
+			pattern: /data=/,
+			parser: function (url) {
+				var data = url.split("data=")[1].split("=")[0];
+				var matches = data.match(/-?\d+\.\d\d+/g);
+				return matches.slice(0, 2).reverse();
+			}
+		}
+	];
+
 	var get_new_ll = function (url) {
-		var data = url.split("data=")[1].split("=")[0];
-		var matches = data.match(/-?\d+\.\d\d+/g);	
-		return matches.slice(0, 2).reverse();	
+		var matching = url_parsers.filter(function (p) {
+			return p.pattern.test(url);
+		});
+		return matching[0].parser(url);
 	};
 
 	dot.onclick = function () {
